ca87a8a3cd1ae3d9c084f43543f5e12cfbccb620
dany-el92 <60103652+dany-el92@users.noreply.github.com>
2021-07-04 21:50:39 +0200

    correzione modifica/eliminazione utente

--- utente.c
+++ utente.c
@@ -21,8 +21,7 @@ struct utente *registrazioneUtente(struct utente *testa) {
     nuovoNodo = (struct utente *) malloc(sizeof(struct utente));
 
     int ultimoID;
-    bool annoBis = false; //flag anno bisestile
-    bool dataCorretta = false; //flag per correttezza verifica data di nascita
+    bool dataCorretta = true; //flag per verificare la correttezza della data di nascita
     bool flag = false; //flag per controllare univocità del campo email e nickname
 
     FILE *fp;
@@ -105,16 +104,21 @@ struct utente *registrazioneUtente(struct utente *testa) {
     strcpy(nuovoNodo->password, psw);
 
     printf("-----------------------------\n");
+    
     //controlli sulla data di nascita
     char data[11];
     int giorno, mese, anno;
-    int etaMinima;
 
     printf("Inserisci data di nascita\n");
 
-    printColor("---Per potersi registrare bisogna avere almeno 16 anni---\n", COLOR_RED);
+    printColor("---Per potersi registrare bisogna avere almeno 16 anni---\n\n", COLOR_RED);
     
     do {
+        if(!dataCorretta) {
+            printColor("\nAttenzione!\n", COLOR_RED);
+            printf("La data inserita non è corretta.\nSi prega di inserirla nuovamente\n\n");
+        }
+        
         do {
             printf("Giorno: ");
             scanf("%d", &giorno);
@@ -128,55 +132,13 @@ struct utente *registrazioneUtente(struct utente *testa) {
         printf("Anno (dal 1900 in poi): ");
         scanf("%d", &anno);
         
-        //controllo se l'anno inserito è bisestile o meno
-        if (anno % 4 == 0) {
-            if (anno % 100 == 0) {
-                if (anno % 400 == 0) {
-                    annoBis = true;
-                } else {
-                    annoBis = false;
-                }
-            } else {
-                annoBis = true;
-            }
-        } else {
-            annoBis = false;
-        }
-
-        //controllo la correttezza di tutta la data inserita
-        if ((giorno > 28 && mese == 2 && annoBis == false) || (giorno > 29 && mese == 2 && annoBis == true)) {
-            dataCorretta = false;
-        } else {
-            if (giorno > 31 &&
-                (mese == 1 || mese == 3 || mese == 5 || mese == 7 || mese == 8 || mese == 10 || mese == 12)) {
-                dataCorretta = false;
-            } else {
-                if (giorno > 30 && (mese == 4 || mese == 6 || mese == 9 || mese == 11)) {
-                    dataCorretta = false;
-                } else {
-                    dataCorretta = true;
-                }
-            }
-        }
-
-        //controllo la data odierna, per verificare se l'utente ha l'eta minima richiesta per potersi iscrivere
-        time_t now;
-        struct tm *ts;
-        char annoCorrente[5];
-
-        now = time(NULL);
-
-        ts = localtime(&now);
-        strftime(annoCorrente, sizeof(annoCorrente), "%Y", ts);
-
-        // per potersi registrare al sistema bisogna avere almeno 16 anni
-        etaMinima = atoi(annoCorrente) - 16;
-
-        //se l'età è minore a quella richiesta, oppure, se la data di nascita inserita è errata verrà richiesto l'inserimento
-    } while (anno > etaMinima || dataCorretta == false);
+        dataCorretta = verificaData(giorno, mese, anno);
+        
+    } while (!dataCorretta);
+    
+    while ('\n' != getchar());
 
     snprintf(data, 11, "%d/%d/%d", giorno, mese, anno);
-    //data = dataNascita();
     strcpy(nuovoNodo->dataNascita, data);
 
     //verifico se nel file ci sono già utenti registrati o meno
@@ -316,21 +278,19 @@ void stampaUtente(struct utente *utenteLogin) {
 
 struct utente *modificaUtente(struct utente *utenteLogin, struct utente *testa) {
     int scelta;
-    char risposta[3];
+    char risposta;
 
-    char data[10];
+    char data[11];
     int giorno, mese, anno;
-    int etaMinima;
-    bool annoBis = false; //flag anno bisestile
-    bool dataCorretta = false; //flag per correttezza verifica data di nascita
-
+    bool dataCorretta = true; //flag per verificare la correttezza della data di nascita
+    bool flag = false;
+    
     struct utente *temp = NULL;
 
     temp = utenteLogin;
 
     do {
-        while ('\n' != getchar());
-
+        
         //elenco campi modificabili
         printf("----------\n");
         printf("1: Nome\n");
@@ -359,10 +319,28 @@ struct utente *modificaUtente(struct utente *utenteLogin, struct utente *testa) 
                 break;
 
             case 3:
-                printf("Inserisci la nuova Email (non ti dimenticare la @): \n");
-                fgets(temp->email, 60, stdin);
-                temp->email[strlen(temp->email) - 1] = 0;
+                
+                do {
+                    flag = false;
+                    printf("Inserisci Email (non ti dimenticare la @): \n");
+                    fgets(temp->email, 60, stdin);
+                    temp->email[strlen(temp->email) - 1] = 0;
+                    
+                    for (temp = testa; temp != NULL; temp = temp->nextUtente) {
 
+                        if (strcmp(temp->email, temp->email) == 0) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    
+                    if (flag) {
+                        printColor("---Email gia' in uso!---\n", COLOR_RED);
+                        printColor("---Si prega di sceglierne un'altra---\n", COLOR_RED);
+                    }
+                    
+                } while (flag);
+                
                 //verifico che sia stata inserita la @
                 char *ptr;
                 char *a = "@";
@@ -387,81 +365,34 @@ struct utente *modificaUtente(struct utente *utenteLogin, struct utente *testa) 
 
 
             case 5:
-                //prossisamente sarà una funzione (?)
-                //controlli sulla data di nascita
                 printf("Inserisci data di nascita\n");
 
-                printColor("---Per potersi registrare bisogna avere almeno 16 anni---\n", COLOR_RED);
-
+                printColor("---Per potersi registrare bisogna avere almeno 16 anni---\n\n", COLOR_RED);
+                
                 do {
+                    if(!dataCorretta) {
+                        printColor("\nAttenzione!\n", COLOR_RED);
+                        printf("La data inserita non è corretta.\nSi prega di inserirla nuovamente\n\n");
+                    }
+                    
                     do {
                         printf("Giorno: ");
                         scanf("%d", &giorno);
-                        // printf("\n");
                     } while (giorno < 0 || giorno > 31);
 
                     do {
                         printf("Mese: ");
                         scanf("%d", &mese);
-                        // printf("\n");
                     } while (mese < 1 || mese > 12);
 
                     printf("Anno (dal 1900 in poi): ");
                     scanf("%d", &anno);
-                    // printf("\n");
-                    printf("---------------------------------\n");
+                    
+                    dataCorretta = verificaData(giorno, mese, anno);
+                    
+                } while (!dataCorretta);
 
-                    //controllo se l'anno inserito è bisestile o meno
-                    if (anno % 4 == 0) {
-                        if (anno % 100 == 0) {
-                            if (anno % 400 == 0) {
-                                annoBis = true;
-                            } else {
-                                annoBis = false;
-                            }
-                        } else {
-                            annoBis = true;
-                        }
-                    } else {
-                        annoBis = false;
-                    }
-
-                    //controllo la correttezza di tutta la data inserita
-                    if ((giorno > 28 && mese == 2 && annoBis == false) ||
-                        (giorno > 29 && mese == 2 && annoBis == true)) {
-                        dataCorretta = false;
-                    } else {
-                        if (giorno > 31 &&
-                            (mese == 1 || mese == 3 || mese == 5 || mese == 7 || mese == 8 || mese == 10 ||
-                             mese == 12)) {
-                            dataCorretta = false;
-                        } else {
-                            if (giorno > 30 && (mese == 4 || mese == 6 || mese == 9 || mese == 11)) {
-                                dataCorretta = false;
-                            } else {
-                                dataCorretta = true;
-                            }
-                        }
-                    }
-
-                    //controllo la data odierna, per verificare se l'utente ha l'eta minima richiesta per potersi iscrivere
-                    time_t now;
-                    struct tm *ts;
-                    char annoCorrente[5];
-
-                    now = time(NULL);
-
-                    ts = localtime(&now);
-                    strftime(annoCorrente, sizeof(annoCorrente), "%Y", ts);
-
-                    // per potersi registrare al sistema bisogna avere almeno 16 anni
-                    etaMinima = atoi(annoCorrente) - 16;
-
-                    //se l'età è minore a quella richiesta, oppure, se la data di nascita inserita è errata verrà richiesto l'inserimento
-                } while (anno > etaMinima || dataCorretta == false);
-
-                snprintf(data, 10, "%d/%d/%d", giorno, mese, anno);
-                //data = dataNascita();
+                snprintf(data, 11, "%d/%d/%d", giorno, mese, anno);
                 strcpy(temp->dataNascita, data);
                 
                 while ('\n' != getchar());
@@ -471,25 +402,28 @@ struct utente *modificaUtente(struct utente *utenteLogin, struct utente *testa) 
                 break;
         }
         
-        printf("Vuoi modificare un altro campo? (si/no): ");
-        fgets(risposta, 3, stdin);
-
-        //rendo la risposta tutta maiuscola per evitare errori
-        for (int i = 0; i < strlen(risposta); i++) {
-            risposta[i] = toupper(risposta[i]);
-        }
-
-        if (strcmp(risposta, "NO") == 0) {
-            scriviUtenti(testa);
-        }
-
-    } while (strcmp(risposta, "SI") == 0);
+        do {
+            
+            printf("Vuoi modificare un altro campo? (s/n): ");
+            scanf("%c", &risposta);
+            while ('\n' != getchar());
+            
+            //rendo la risposta in maiuscolo per evitare errori
+            risposta = toupper(risposta);
+            
+            if (risposta == 'N') {
+                scriviUtenti(testa);
+            }
+            
+        } while(risposta != 'S' && risposta != 'N');
+        
+    } while (risposta == 'S');
 
     return testa;
 }
 
 struct utente *eliminaUtente(struct utente *utenteLogin, struct utente *testa) {
-    char risposta[3];
+    char risposta;
     struct utente *curr, *prec;
     struct utente *temp = NULL;
 
@@ -498,22 +432,23 @@ struct utente *eliminaUtente(struct utente *utenteLogin, struct utente *testa) {
     curr = testa;
     
     do {
-        while ('\n' != getchar());
+        //while ('\n' != getchar());
         printColor("ATTENZIONE!\n", COLOR_RED);
         printf("Sei sicuro/a di voler eliminare il tuo account?\n");
         printf("Per poter riaccedere dovrai creare un nuovo account\n");
-        printf("Risposta (si/no): ");
-        fgets(risposta, 3, stdin);
+        printf("Risposta (s/n): ");
+        scanf("%c", &risposta);
         printf("\n");
+        while ('\n' != getchar());
         
-        //rendo la risposta tutta maiuscola per evitare errori
-        for (int i = 0; i < strlen(risposta); i++) {
-            risposta[i] = toupper(risposta[i]);
-        }
+        //rendo la risposta in maiuscolo per evitare errori
+        risposta = toupper(risposta);
         
-    }while(strcmp(risposta, "SI") != 0 && strcmp(risposta, "NO") != 0);
+    }while(risposta != 'S' && risposta != 'N');
     
-    if (strcmp(risposta, "SI") == 0) {
+    
+    
+    if (risposta == 'S') {
         
         while (curr != NULL && temp->id != curr->id) {
             prec = curr;
@@ -556,9 +491,9 @@ void scriviUtenti(struct utente *testa) {
         }
     }
     
+    fclose(fp);
     remove("utenti.csv");
     rename("utentiTemp.csv", "utenti.csv");
-    fclose(fp);
 }
 
 //ordinamento lista
